4779fd9e75be18f6fbe1574f404caba69b7746b4
jackbaude <jackbaude@gmail.com>
2018-11-25 21:25:58 -0600

    updated date

--- server-client/Client.java
+++ server-client/Client.java
@@ -10,24 +10,16 @@ import java.util.*;
  */
 public class Client {
 
-    /**
-     * Runs the client as an application.  First it displays a dialog
-     * box asking for the IP address or hostname of a host running
-     * the date server, then connects to it and displays the date that
-     * it serves.
-     */
+    
     public static void main(String[] args) throws IOException {
         Scanner scan = new Scanner(System.in);
-    	System.out.println(
-            "Enter IP Address of a machine that is\n" +
-            "running the date service on port 9090:");
+    	System.out.println("Enter IP Address of a machine that is \n" + "running the date service on port 9090:");
     	String serverAddress = scan.nextLine();
     	scan.close();
         
     	
     	Socket s = new Socket(serverAddress, 9090);
-        BufferedReader input =
-            new BufferedReader(new InputStreamReader(s.getInputStream()));
+        BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));
         String answer = input.readLine();
         System.out.println(answer);
         
--- server-client/Server.java
+++ server-client/Server.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Date;
+
 
 //Sends date and time to client.
 
@@ -10,12 +10,14 @@ public class Server {
     public static void main(String[] args) throws IOException {
         ServerSocket listener = new ServerSocket(9090);
         String hello = "Hello from the Server!";
+        java.util.Date date=new java.util.Date();  
+		
         try {
             while (true) {
                 Socket socket = listener.accept();
                 try {
                     PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-                    out.println(new Date().toString());
+                    out.println(date.toString());
                     out.println(hello);
                 } finally {
                     socket.close();
