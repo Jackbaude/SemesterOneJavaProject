80f407d46cd0651622947fa78af58b6c41ad4572
Jack <Jackbaude@gmail.com>
2018-11-28 20:53:21 -0600

    New Dashboard with staic data ATM. look into updating to active data

--- GUI/ServerGUI.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.JFrame;
-class ServerGUi{
-	public static void main(String[] args) {
-		JFrame frame = new JFrame("FrameDemo");
-		
-		
-	}
-}
---
+++ server-client/ServerGUI.java
@@ -0,0 +1,32 @@
+import javax.swing.JFrame;
+import javax.swing.JTable;
+import javax.swing.JScrollPane;
+
+class ServerGUi{
+	public static JFrame dashboard = new JFrame("Dashboard");
+	
+	public static Object[][] data = new Object[][] {
+        {1, "1.0.0.1", 40.0},
+        {2, "1.0.0.1", 70.0},
+        {3, "1.0.0.1", 60.0},
+    };
+    public static String[] columns = new String[] {
+            "Number", "I.P", "UP TIME"};
+    public static JTable table = new JTable(data, columns); 
+	//////////////////MAIN//////////////////////
+    public static void main(String[] args) {
+		dashboardWindowMake();
+		
+	}	
+	
+    public static void dashboardWindowMake() {
+		dashboard.setVisible(true);	
+		dashboard.setTitle("Dashboard Information");
+		dashboard.setBounds((960 - 250), (540 - 250), 500, 500);
+		dashboard.add(new JScrollPane(table));
+		
+		dashboard.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		dashboard.pack();
+		
+	}
+}
