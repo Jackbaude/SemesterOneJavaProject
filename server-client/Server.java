e8d9c3c674852f5de9b2c98a8d14ce9ca19a8b2b
Jack <Jackbaude@gmail.com>
2018-12-22 21:54:42 -0600

    Update Server.java

--- server-client/Server.java
+++ server-client/Server.java
@@ -6,11 +6,6 @@ import java.net.Socket;
 //Sends date and time to client.
 public class Server {
     public static void main(String[] args) throws IOException {
-    	
-    	
-    	
-    	
-    	
     	ServerSocket listener = new ServerSocket(9090);
         String hello = "Hello from the Server!";
         java.util.Date date=new java.util.Date();  
