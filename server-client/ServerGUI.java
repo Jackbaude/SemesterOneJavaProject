86006e1982428bdcaa6374933c574a01b39ef5e0
Jack <Jackbaude@gmail.com>
2018-12-22 21:54:16 -0600

    Update ServerGUI.java

--- server-client/ServerGUI.java
+++ server-client/ServerGUI.java
@@ -1,32 +1,27 @@
 import javax.swing.JFrame;
-import javax.swing.JTable;
 import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
-class ServerGUi{	
-	public static Object[][] data = new Object[][] {
-        {1, "1.0.0.1", 40.0},
-        {2, "1.0.0.1", 70.0},
-        {3, "1.0.0.1", 60.0},
-        //Need to figure out how to add a row automatically.
-    };
-    public static String[] columnHeaders = new String[] {
+public class ServerGUi {
+	public static  Object[][] data = new Object[1000][1000];;
+	public static String[] columnHeaders = new String[] {
             "Number", "I.P", "UP TIME"};
-    
-    
-	//Main
-    public static void main(String[] args) {
-		dashboardWindowMake();
-	}
-    //Window Make
-    public static void dashboardWindowMake() {
-    	JFrame dashboard = new JFrame("Dashboard");
+	public static void main(String[] args) {
+		JFrame dashboard = new JFrame("Dashboard");
     	dashboard.setVisible(true);	
 		dashboard.setTitle("Dashboard Information");
 		dashboard.setBounds((960 - 250), (540 - 250), 500, 500);
-		dashboard.add(new JScrollPane(table));
 		JTable table = new JTable(data, columnHeaders); 
+		dashboard.add(new JScrollPane(table));
+		
+		for (int c = 0; c<=999; c++) {
+			for (int r = 0; r==0; r++) {
+				data[c][r] = c;		}
+		}
+		data[0][1] = Client.ipaddress();
+		
 		dashboard.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		dashboard.pack();
-		
 	}
+
 }
