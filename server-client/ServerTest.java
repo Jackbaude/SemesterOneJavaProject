c87aaa2fb7f596fa8c51b59d91fe69e168196e0a
jack <jack@JackLinuxMachine>
2018-12-26 18:24:22 -0600

    for testing

---
+++ server-client/ServerTest.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+public class ServerTest {
+	public static void main(String[] args) throws IOException{
+		String welcome = "Welcome";
+		try (var listener = new ServerSocket(9090)) {
+            System.out.println("Server is running");
+            while (true) {
+                try (var socket = listener.accept()) {
+                    var out = new PrintWriter(socket.getOutputStream(), true);
+                    out.println(welcome.toString());
+                 
+                }
+                if (1 == 1) {
+                	break;
+                }
+                finally {
+                	listener.close();
+                	
+                }
+            }
+        }
+
+	}
+
+}
