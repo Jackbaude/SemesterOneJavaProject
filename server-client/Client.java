3d41ab408583b0c37eeb79176aac501d5b1bc5d8
Jack <Jackbaude@gmail.com>
2018-12-27 15:28:12 -0600

    Simplified and thought threw - very close to done

--- server-client/Client.java
+++ server-client/Client.java
@@ -1,34 +1,27 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.URL;
+import java.time.LocalDateTime;
 import java.util.*;
 
 public class Client {
-	public static int vmNumber = Server.NumVMs;
+	//public static int vmNumber = Server.NumVMs;
 	public static String ipin() {
 		Scanner scanip = new Scanner(System.in);
 		String ip = scanip.nextLine();
 		scanip.close();
 		return ip;
 	}
-    public static String systemipaddress = "";
-    public static void main(String[] args) throws IOException {
-    	System.out.println("Enter IP Address of a machine that is \n" + "running the date service on port 9090:");
-    	Socket s = new Socket(ipin(), 9090);
-        BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));
-        String welcome = input.readLine();
-        System.out.println(welcome);
-        
-        
-    }
+   
     public static String ipaddress() {
         try 
         {
             URL url_name = new URL("http://bot.whatismyipaddress.com");
             BufferedReader sc = new BufferedReader(new InputStreamReader(url_name.openStream()));
-            systemipaddress = sc.readLine().trim();
+            String systemipaddress = sc.readLine().trim();
             return systemipaddress;   
         }    
         catch (Exception e2) 
@@ -36,21 +29,30 @@ public class Client {
             return "Cannot get IP address";
         }   
     }
-    public static long timer() {
-    	long start = System.currentTimeMillis();
-    	long stop = System.currentTimeMillis();
-    	long elapsed = stop - start;
-    	return elapsed;
+    public static String time() {
+    	LocalDateTime uptime = LocalDateTime.now();
+    	return (uptime.toString());
     }
-    
-    
-    public static void virtualmachine() {
-    	
-    	while (true) {
-    		timer();
+    public static void main(String[] args) throws IOException {
+    	System.out.println("Enter IP Address of a machine that is \n" + "running the VM service on port 9090:");
+    	Socket s = new Socket(ipin(), 9090);
+        BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        String welcome = input.readLine();
+        System.out.println(welcome);
+        
+        //collect ip address
+        String systemipaddress = ipaddress();
+		//collect time 
+		String time = time();
+		
+		PrintWriter out = new PrintWriter(s.getOutputStream(), true);
+        out.println(systemipaddress);
+        out.println(time);
+        //start VM
+        /*if (somthing){
+        	shutdown the VM
     	}
-    	
-    	
+    */
     }
     
 }
