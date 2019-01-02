41f161af6883f13c99f0f98e695c0e46911a2b0d
jack <jack@JackLinuxMachine>
2019-01-02 13:38:18 -0600

    Trys 10 times to connect and if it cant then stops

--- server-client/Client.java
+++ server-client/Client.java
@@ -2,9 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.InetAddress;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import java.time.LocalDateTime;
 
 public class Client {
@@ -13,24 +11,14 @@ public class Client {
     	LocalDateTime uptime = LocalDateTime.now();
     	return (uptime.toString());
     }
-    public static void main(String[] args) throws IOException, InterruptedException {
-    	System.out.println("Client Running");
+    public static Socket s;
+    public static void connect() throws InterruptedException {
     	int connattempts = 0;
     	while (connattempts <= 10) {
 	    	try{
-	    		Socket s = new Socket("192.168.122.1", 9090);
+	    		s = new Socket("192.168.122.1", 9090);
+	    		return;
 	    		//Exception would be caught right here
-	        	BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));
-	        	//shows that they have connected
-	            String welcome = input.readLine();
-	            System.out.println(welcome);
-	            
-	    		//collect time 
-	    		String time = time();
-	    		PrintWriter out = new PrintWriter(s.getOutputStream(), true);
-	            out.println(time);
-	            s.close();
-	            //We no longer need the java side here because the VM has already checked in
 	    	}
 	    	catch(Exception e) {
 	    		connattempts++;
@@ -39,8 +27,23 @@ public class Client {
     	}
     	if (connattempts >= 10) {
     		System.out.println("Sorry the server is probaly not up or you have the wrong ip");
-    		return;
+    		System.exit(0);;
     	}
+    }
+    public static void main(String[] args) throws IOException, InterruptedException {
+    	connect();
+    	System.out.println("Client Running");
+    	
+    	BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));
+    	//shows that they have connected
+        String welcome = input.readLine();
+        System.out.println(welcome);
+        
+		//collect time 
+		String time = time();
+		PrintWriter out = new PrintWriter(s.getOutputStream(), true);
+        out.println(time);
+        s.close();
     }
   
 }
